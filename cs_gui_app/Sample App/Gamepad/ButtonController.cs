5a11eda196b7be0f3a056a67abb22b282e81b9c4
AJIOB <aleksandr9809@gmail.com>
2018-04-24 12:14:02 +0300

    DigitalController implementation

--- cs_gui_app/Sample App/Gamepad/ButtonController.cs
+++ cs_gui_app/Sample App/Gamepad/ButtonController.cs
@@ -2,9 +2,62 @@
 {
     public class ButtonController : IUpdatable
     {
+        private const bool DefaultValue = false;
+
+        public delegate void DigitalCallback();
+        public delegate bool? GetDigitalValueIsPressed();
+
+        public event DigitalCallback Pressed;
+        public event DigitalCallback Released;
+        public event DigitalCallback Toggled;
+        public event GetDigitalValueIsPressed ReadValue;
+
+        private bool _oldValueIsPressed = DefaultValue;
+
         public void Update()
         {
-            throw new System.NotImplementedException();
+            bool newValue = OnReadValue();
+            bool oldValue = _oldValueIsPressed;
+            _oldValueIsPressed = newValue;
+            if (oldValue == newValue)
+            {
+                return;
+            }
+
+            OnToggled();
+
+            if (newValue)
+            {
+                OnPressed();
+            }
+            else
+            {
+                OnReleased();
+            }
         }
+
+        #region Event Invocators
+
+        protected virtual void OnPressed()
+        {
+            Pressed?.Invoke();
+        }
+
+        protected virtual void OnReleased()
+        {
+            Released?.Invoke();
+        }
+
+        protected virtual void OnToggled()
+        {
+            Toggled?.Invoke();
+        }
+
+        protected virtual bool OnReadValue()
+        {
+            return ReadValue?.Invoke() ?? DefaultValue;
+        }
+
+        #endregion
     }
 }
